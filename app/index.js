98e87febf95b08ad776076ac161bc6dcd693de06
Daniel <daniel.e.zyubach@gmail.com>
2023-07-15 19:42:15 +0300

    styles

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native"
+import styles from "../styles/style"
 
 const Home = () => {
   return (
     <View>
-      <Text>Hello, world!</Text>
+      <Text style={styles.bigText}>Hello, world!</Text>
     </View>
   )
 }
