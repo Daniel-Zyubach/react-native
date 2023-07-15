5425dc3bac5f16071d8afe6773300ccc5032f80e
Daniel <daniel.e.zyubach@gmail.com>
2023-07-16 00:04:23 +0300

    ISLOADING

--- app/index.js
+++ app/index.js
@@ -1,16 +1,21 @@
 //работа с API
 import axios from "axios"
 
-import { View, Alert, FlatList } from "react-native"
+import { View, Alert, FlatList, ActivityIndicator, Text } from "react-native"
 import { useState, useEffect } from "react"
 
 import Post from "../components/Post"
 
 const App = () => {
+  //оповещение пользователя об загрузке данных
+  const [isLoading, setIsLoading] = useState(true)
+
   //state для хранения статей
   const [items, setItems] = useState()
-  //отправка запроса и проверка при рендеринге
-  useEffect(() => {
+
+  //просто делает запрос
+  const fetchPosts = () => {
+    setIsLoading(true)
     //отправляем запрос
     axios.get('../articles.json')
     //вытаскиваем из axios получаемые данные и помещаем их в setItems
@@ -18,8 +23,28 @@ const App = () => {
     //если вдруг произойдёт ошибка при получении данных, то
     }).catch((err) => {
       Alert.alert('Ошибка', 'Не удалось получить статьи')
+    //когда запрос выполнился или вернул ошибку
+    }).finally(() => {
+      setIsLoading(false)
     })
-  }, [])
+  }
+
+  //отправка запроса и проверка при рендеринге
+  useEffect(fetchPosts, [])
+
+  //условие для показа индикатора загрузки
+  if (isLoading) {
+    return <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <ActivityIndicator size={"large"} />
+      <Text style={{fontSize: 17, fontWeight: 600, marginTop: 15}}>Загрузка...</Text>
+    </View>
+  }
 
   return (
     <View>
